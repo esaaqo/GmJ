eb6285c38d178fcbe4a051b1738b02b4a7abdcb0
esaaqo <esaaq@gmail.com>
2016-12-28 21:16:52 -0500

    second Commit, test commit

---
+++ src/test/java/gmj/isc/com/TST1.java
@@ -0,0 +1,13 @@
+package gmj.isc.com;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TST1 {
+   @Test
+	
+   public void testAdd() {
+      String str = "Junit is working fine";
+      assertEquals("Junit is working fine",str);
+   }
+}
